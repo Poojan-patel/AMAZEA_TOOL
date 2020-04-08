10f199edb97079b7455e53a32f3b9d320f0c9500
Poojan-patel <56884359+Poojan-patel@users.noreply.github.com>
2020-04-08 14:15:37 +0530

    Create amazea.py

---
+++ amazea.py
@@ -0,0 +1,37 @@
+import cv2
+import webrecognize
+import sys
+from tkinter import *
+import tkinter as tk 
+
+def face_reco_done_or_not():
+	args=["","frontfacial.xml","./Known/"]
+	name, frame = webrecognize.function_for_face_reco(args)
+	'''if(name == 'No'):
+		name = input("Enter Your Name:")
+		cv2.imwrite(filename=name, img=frame)
+	else:
+		print("Found")'''
+
+def fun():
+	video_capture = cv2.VideoCapture(0)
+
+	while True:
+	    ret, frame = video_capture.read()
+	    cv2.imshow('Face_Reco', frame)
+	    if (cv2.waitKey(1) & 0xFF == ord('q')):
+	        break
+	video_capture.release()
+	cv2.destroyAllWindows()
+
+mw = tk.Tk()
+mw.title('AMAZEA')
+mw.configure(background='turquoise')
+mw.geometry("500x500")
+lbl = tk.Label(mw, text='Welcome to Lightning Parties!', fg='purple', bg='turquoise', font=('comicsans', 14))
+lbl.pack()
+
+btn = tk.Button(mw,text="Face Recognition",command=face_reco_done_or_not)
+btn.pack()
+
+mw.mainloop()
