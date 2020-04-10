5770a506ddc34add8eb9c46538f0552dddd587f2
stdnoob <51433148+shivansh6@users.noreply.github.com>
2020-04-10 22:47:43 +0530

    Add files via upload

---
+++ amazea_updated.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[96]:
+
+
+import cv2
+import webrecognize
+import sys
+from tkinter import *
+import tkinter as tk 
+import signup as sp
+def face_reco_done_or_not():
+    mw.destroy()
+    args=["","frontfacial.xml","./Known/"]
+    name, frame = webrecognize.function_for_face_reco(args)
+    if(name == 'No'):
+        var = sp.main()
+        cv2.imwrite(filename=var, img=frame)
+    else:
+        print("Found")
+
+def fun():
+    video_capture = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = video_capture.read()
+        cv2.imshow('Face_Reco', frame)
+        if (cv2.waitKey(1) & 0xFF == ord('q')):
+            break
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+mw = tk.Tk()
+mw.title('AMAZEA')
+mw.configure(background='turquoise')
+mw.geometry("500x500")
+lbl = tk.Label(mw, text='Welcome to Lightning Parties!', fg='purple', bg='turquoise', font=('comicsans', 14))
+lbl.pack()
+
+btn = tk.Button(mw,text="Face Recognition",command=face_reco_done_or_not)
+btn.pack()
+
+mw.mainloop()
+
+
+# In[94]:
+
+
+import signup as sp
+x=sp.main()
+print(x)
+
---
+++ signup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+from tkinter import *
+import tkinter as tk
+def main():
+    def get1(*args):
+        global ans
+        name = v1.get()
+        ans=name
+        #print(name)
+    def save():
+        global ans
+        if(not ".jpg" in ans):
+            ans+=".jpg"
+        #print(ans)
+    def fsnq():
+        global ans
+        if(not ".jpg" in ans):
+            ans+=".jpg"
+        #print(ans)
+        mw1.destroy()
+    mw1 = tk.Tk()
+    mw1.title("Sorry! You are not found...")
+    mw1.configure(background='aquamarine2')
+    mw1.geometry("500x200")
+    v1 = StringVar()
+    e = tk.Entry(mw1,textvariable=v1,bg='lightblue',bd=4,font=10)
+    l1 = tk.Label(mw1,bg='aquamarine2',text = "Enter Your Name :- ",font=10)
+    l1.place(x=40,y=40)
+    e.place(x=220,y=40)
+    btn=tk.Button(mw1,text="Save",command=save,bg='green',font=5)
+    btn.place(x=150,y=105)
+    quit=tk.Button(mw1,text="Quit",command=mw1.destroy,bg='red',font=5)
+    quit.place(x=300,y=105)
+    snq=tk.Button(mw1,text="Save&Quit",command=fsnq,bg='salmon1',font=5)
+    snq.place(x=200,y=150)
+    v1.trace_add("write", get1)
+    mw1.mainloop()
+    return ans
+s=main()
+print(s)
+
